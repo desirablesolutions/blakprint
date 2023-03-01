bdc27eb7e975b441fd42573d0385868298c058bb
Jeffrey J. Desir <devops@informandinspire.llc>
2023-03-01 01:45:45 -0500

    d

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
