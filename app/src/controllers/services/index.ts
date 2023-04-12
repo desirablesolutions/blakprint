1627591d68b79f607594478bbf9c401f76f8a0af
Jeffrey Desir <jeffrey@desir.tech>
2023-04-12 06:59:16 -0400

    321

--- app/src/controllers/services/index.ts
+++
@@ -1,9 +0,0 @@
-import services from "blakprint/services"
-import NotionService from "bp-notion-service"
-
-const { defineServices, defineService } = services()
-
-export const ApplicationServices = defineServices({
-    NotionService,
-    
-})
