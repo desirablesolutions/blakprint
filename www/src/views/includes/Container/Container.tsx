8267d8cdb6ec4ef9b113bd8e656351fb158d29ac
Jeffrey Desir <jeffrey@desir.tech>
2023-07-26 13:12:28 -0400

    999

--- www/src/views/includes/Container/Container.tsx
+++ www/src/views/includes/Container/Container.tsx
@@ -2,7 +2,7 @@ export default function Container(props: any) {
 
     const { children } = props
     return (
-        <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-white bg-opacity-25">
+        <main className="flex flex-col items-center justify-between min-h-screen p-24 overflow-x-hidden bg-white bg-opacity-25">
     {children}
       </main>
     )
